684c208c8f6126fd1365e19235a34f6568a33c64
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-03-28 17:18:52 -0300

    added connection to main

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from group1.blocks import Block
 from group1 import  manage_database as manager
 
 
-
+if __name__ == "__main__":
+    db = manager.get_connection()
+    print(db.list_collection_names())
