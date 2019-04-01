3856e3dd89226fece2961c3bd22a1933c2eafd78
Alfonso <ajzuniga1@miuandes.cl>
2019-04-01 11:09:27 -0300

    Fixed bug in the search function

--- main.py
+++ main.py
@@ -2,20 +2,24 @@ from blocks import Block
 import manage_database as manager
 from arguments_manager import manage_arguments
 from file_parser import parse_file
-
+from bson.json_util import dumps
 
 
 def main(args):
 	db = manager.get_connection()
-	print(db.list_collection_names())
+	# print(db.list_collection_names())
 	if args.insert:
 		# data_blocks would be an array that is to be traveled and each element should be loaded to the database
 		data_blocks = parse_file(args)
 		for block in data_blocks:
-			manager.insert_one_block(db,block)
+			manager.insert_one_block(db, block)
 	else:
-		id = args.id
-		block = manager.find_by_id(db,id)
+		if args.marvin:
+			block = manager.find_by_id(db, args.id, "Marvin")
+			manager.print_block(block.next())
+		else:
+			block = manager.find_by_id(db, args.id, "Zuck small")
+			manager.print_block(block.next())
 		# here you should fetch the block from the database that has this id
 
 if __name__ == "__main__":
--- manage_database.py
+++ manage_database.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import connection_params
+from blocks import Block
 
 def get_connection():
     connection = MongoClient(
@@ -8,8 +9,14 @@ def get_connection():
     )
     return connection.mining_blocks
 def insert_one_block(db, block):
-
     db.blocks.insert_one(block.as_json())
 
-def find_by_id(db, id):
-    return db.find({"id": id })
+def find_by_id(db, id, model):
+    return db.blocks.find({"id": id, "model": model })
+
+def get_model_blocks(db, model):
+	return db.blocks.find({ "model": model })
+
+def print_block(response):
+	block = Block(response['model'], response['id'], response['x'], response['y'], response['z'], response['weight'], response['grade'])
+	print(block)
