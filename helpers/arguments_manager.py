08650b94adda72cc847dff3ebce45be80ecd2b99
angelesaldunate <31292145+angelesaldunate@users.noreply.github.com>
2019-05-12 17:32:21 -0400

    Fixed long function on argument manager

--- helpers/arguments_manager.py
+++ helpers/arguments_manager.py
@@ -13,12 +13,7 @@ def manage_arguments():
 	block_model.add_argument("-bm", "--block_model", help="Name of the block model that is to me considered within the context of the provided mineral deposit. Leave blank if you are to insert a new block model")
 	
 	# Action to be exectuted
-	action = parser.add_mutually_exclusive_group(required=True)
-	action.add_argument("-i", "--insert", help="Indicates that you want to insert data into the database", action="store_true")
-	action.add_argument("-s", "--search", help="Indicates that you want to search for specific block within the given block model", action="store_true")
-	action.add_argument("-r", "--remove", help="Indicates that you want to remove all blocks belonging to the provided block model", action="store_true")
-	action.add_argument("-m", "--metrics", help="Indicates that you want to fetch and analyse data from a given mineral deposit and block model", action="store_true")
-
+	action_to_execute(parser)
 	# Directory of the file containing the data to be uploaded to the server
 	file_input = parser.add_argument_group("file_input")
 	file_input.add_argument("-f", "--file_input", help="Directory of the file containing the data to be uploaded to the server")
@@ -28,3 +23,10 @@ def manage_arguments():
 	coordinates.add_argument("-c", "--coordinates", help="Coordinates X, Y and Z of the desired block, separated only by commas")
 
 	return parser.parse_args()
+
+def action_to_execute(parser):
+	action = parser.add_mutually_exclusive_group(required=True)
+	action.add_argument("-i", "--insert", help="Indicates that you want to insert data into the database",action="store_true")
+	action.add_argument("-s", "--search",help="Indicates that you want to search for specific block within the given block model",action="store_true")
+	action.add_argument("-r", "--remove",help="Indicates that you want to remove all blocks belonging to the provided block model",action="store_true")
+	action.add_argument("-m", "--metrics",help="Indicates that you want to fetch and analyse data from a given mineral deposit and block model",action="store_true")
